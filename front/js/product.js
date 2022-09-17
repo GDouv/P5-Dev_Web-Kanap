43527304777da3a22b3609a76716bad9dcdeeeb6
Guillaume Douv <gui.douvrin@gmail.com>
2022-09-17 12:25:52 +0200

    added name, price and description on product page

--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,9 @@
 let url = 'http://localhost:3000/api/products';
 
 let productImg = document.querySelector(".item__img img")
+let productTitle = document.getElementById("title")
+let productPrice = document.getElementById("price")
+let productDescription = document.getElementById("description")
 
 
 let pageUrl = new URL(document.location.href)
@@ -17,9 +20,14 @@ console.log(productId)
 fetch(url + "/" + productId)
 .then(res => res.json())
 .then((currentProduct) => {
+    console.log(currentProduct)
     console.log(currentProduct.imageUrl)
     productImg.setAttribute("src", currentProduct.imageUrl)
+    productTitle.innerText = currentProduct.name
+    productPrice.innerText = currentProduct.price
+    productDescription.innerText = currentProduct.description
+
+    // Ajouter maintenant les couleurs dans le menu déroulant
+
 })
 .catch(err => console.log("Erreur", err));
-
-// test commit
