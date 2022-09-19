edd2c4daae9f5c7a8f5f3d4e02f2e5336f3a03bc
Guillaume Douv <gui.douvrin@gmail.com>
2022-09-19 11:24:08 +0200

    Ajout des informations des produits sur la page product.

--- front/js/product.js
+++ front/js/product.js
@@ -8,26 +8,53 @@ let productDescription = document.getElementById("description")
 
 let pageUrl = new URL(document.location.href)
 
-console.log(pageUrl)
-console.log(pageUrl.href)
-
 pageHref = new URL(pageUrl.href);
 let search_params = new URLSearchParams(pageHref.search)
 let productId = search_params.get('id');
-
-console.log(productId)
 
 fetch(url + "/" + productId)
 .then(res => res.json())
 .then((currentProduct) => {
     console.log(currentProduct)
-    console.log(currentProduct.imageUrl)
     productImg.setAttribute("src", currentProduct.imageUrl)
     productTitle.innerText = currentProduct.name
     productPrice.innerText = currentProduct.price
     productDescription.innerText = currentProduct.description
 
-    // Ajouter maintenant les couleurs dans le menu déroulant
+    let chooseColorText = document.querySelector("#colors option").innerText
+
+    for (let i = 0; i <= 2; i++) {
+
+        let getExistingColors = document.querySelector("#colors :nth-child(" + [i+1] + ")")
+        let currentProductColor = currentProduct.colors[i-1]
+        getExistingColors.innerText = currentProductColor
+        if (i > 0) {
+            getExistingColors.setAttribute("value", currentProductColor.toLowerCase())
+        }
+    }
+
+    document.querySelector("#colors option").innerText = chooseColorText
+
+    if (currentProduct.colors.length > 2) {
+        let createOption = document.createElement('option')
+        let newOption = document.getElementById('colors').appendChild(createOption)
+        newOption.innerText = currentProduct.colors[2]
+        newOption.setAttribute("value", currentProduct.colors[2].toLowerCase())
+    }
+
+    if (currentProduct.colors.length > 3) {
+        let createOption = document.createElement('option')
+        let newOption = document.getElementById('colors').appendChild(createOption)
+        newOption.innerText = currentProduct.colors[3]
+        newOption.setAttribute("value", currentProduct.colors[3].toLowerCase())
+    }
+    /*
+    - La boucle retire le "SVP sélectionner une couleur" du menu déroulant et le remplace par "undefined"
+    lors du 2ème passage dans la boucle, juste avant l'incrémentation.
+    - Peut-être peut-on essayer de remplacer les attributs "value" au lieu du innerText ?
+    - 3ème couleur non prise en compte quand il y a une troisième couleur.
+    - Utiliser le débugeur.
+    */
 
 })
 .catch(err => console.log("Erreur", err));
